005f4d29f4ec686b8fedbd0081f03a890b78c1da
AmeeQAQ <xivsagitemperance@gmail.com>
2024-07-05 16:14:25 +0200

    removed unused rule

--- rc.lua
+++ rc.lua
@@ -373,15 +373,6 @@ awful.rules.rules = {
     -- Set Firefox to always map on the tag named "2" on screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { screen = 1, tag = "2" } },
-	{ rule_any = {
-		name = {
-			"HearthstoneOverlay",
-			},
-		},
-		properties = {
-			ontop = true,
-		}
-	},
 }
 -- }}}
 
